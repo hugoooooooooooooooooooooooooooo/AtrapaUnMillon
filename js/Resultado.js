9d171056d2042a83e037a255e1875af55b1ff3c3
hugoooooooooooooooooooooooooooo <hugoperezgarcia2004@hotmail.com>
2023-11-27 11:06:20 +0100

    Lunes

--- js/Resultado.js
+++ js/Resultado.js
@@ -3,31 +3,56 @@ window.addEventListener("load", mostrarTodo);
 function mostrarTodo() {
     var jugadoresGuardados = localStorage.getItem("jugadores");
     var jugadores = JSON.parse(jugadoresGuardados);
-    var ranking = document.getElementById("ranking");
+    var rankingFacil = document.getElementById("facil");
+    var rankingMedio = document.getElementById("media");
+    var rankingDificil = document.getElementById("dificil");
 
+    //Da la enhorabuena si te has llevado algo de dinero, si no te dice que has perdido
     if(jugadores[jugadores.length -1].dinero > 0){
         document.getElementById("resultado").textContent = "¡¡Enhorabuena has ganado!!, te llevas " + jugadores[jugadores.length -1].dinero + " mil euros.";
     }else{
         document.getElementById("resultado").textContent = "Vaya.., has perdido. ¡Intentalo de nuevo!";
     }
 
+    //si hay jugadores muestra el ranking    
     if(jugadores.length > 0){
-        jugadores.sort(function(a, b) {
-            if (b.dinero - a.dinero !== 0) {
-                return b.dinero - a.dinero;
-            }
-        });
+        ordenarArray();
 
         for(var i = 0; i < jugadores.length;i++){
             var jugador = jugadores[i];
-            var jugadorElement = document.createElement("div");
-            jugadorElement.textContent = `Nombre: ${jugador.nombre}, Dificultad: ${jugador.dificultad}, Dinero: ${jugador.dinero}K`;
-            ranking.appendChild(jugadorElement);
+            var jugadorElement = document.createElement("li");
+            jugadorElement.textContent = `Nombre: ${jugador.nombre}, Dificultad: ${jugador.dificultad}, Dinero: ${jugador.dinero} K`;
+
+            if(jugador.dificultad.toLowerCase() == "facil"){
+                rankingFacil.appendChild(jugadorElement);
+            }else if(jugador.dificultad.toLowerCase() == "media"){
+                rankingMedio.appendChild(jugadorElement);
+            }else if(jugador.dificultad.toLowerCase() == "dificil"){
+                rankingDificil.appendChild(jugadorElement);
+            }else{
+                console.log("No esta metiendo al jugador en ningun tipo de ranking");
+            }
         }
     }else{
         console.log("No hay jugadores");
     }
 
+    function ordenarArray() {
+        var n = jugadores.length;
+        var aux;
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = 0; j < n - i - 1; j++) {
+                if (jugadores[j + 1].dinero > jugadores[j].dinero) {
+                   
+                    aux = jugadores[j + 1];
+                    jugadores[j + 1] = jugadores[j];
+                    jugadores[j] = aux;
+                }
+            }
+        }
+    }    
+
+    //boton para volver a jugar
     document.getElementById("volverJugar").addEventListener("click",()=>{
         window.location.href = "../html/Inicio.html";
     });
