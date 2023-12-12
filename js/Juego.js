3baa92caa65c59b8d973bbafec4a48e8f2d6640b
Hugo Perez <hugoperezgarcia2004@hotmail.com>
2023-12-12 13:21:46 +0100

    Final

--- js/Juego.js
+++ js/Juego.js
@@ -467,12 +467,21 @@ function volumenMas(){
 }
 
 //Aunque se llame playPause no para ni vuelve a reproducir la musica, sino que la mutea y la desmutea
-function playPause(){
-    if(musicaFondo.muted){
+function playPause() {
+    var playPauseElement = document.getElementById("playPause");
+    var svg;
+
+    if (musicaFondo.muted) {
         musicaFondo.muted = false;
-        document.getElementById("playPause").firstElementChild.src = "../icons/mute.svg";
-    }else{
+        svg = '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>';
+    } else {
         musicaFondo.muted = true;
-        document.getElementById("playPause").firstElementChild.src = "../icons/noMute.svg";
+        svg = '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>';
     }
+
+    var nuevoSvg = new DOMParser().parseFromString(svg, 'image/svg+xml').documentElement;
+
+    playPauseElement.innerHTML = '';
+    playPauseElement.appendChild(nuevoSvg);
 }
+
